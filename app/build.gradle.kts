f0752ae120625f50c859fdf8cb7b54e7e2c88187
filou <filoudecagnes@gmail.com>
2026-06-11 13:55:02 +0200

    Bump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.pointex.kiosklauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.$versionCode"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
